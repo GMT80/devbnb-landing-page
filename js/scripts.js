8086cf9522af432c9bad94385d28e8869a5a2124
gmt <gianmaria.tarsi@gmail.com>
2020-05-08 12:00:13 +0200

    Complete
    Add js and animation

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+
+const floatingBTN = document.querySelector('.footerBtn');
+
+
+floatingBTN.addEventListener('click', e => {
+    const footer = document.querySelector('.footer');
+
+    if (footer.classList.contains('active')) {
+        footer.classList.remove('active');
+        e.target.innerText = 'Terms, Privacy, Currency & more';
+    } else {
+        footer.classList.add('active');
+        e.target.innerText = 'X Close';
+    }
+})
